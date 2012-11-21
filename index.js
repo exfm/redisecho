3339688266a0d5b24b775f4ecf1ce5aac09200e6
Drew Harris <contac@grmnygrmny.com>
2012-11-21 12:54:32 -0500

    first real commit

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var redis = require('redis'),
+	nconf = require('nconf'),
+	fs = require('fs');
+
+nconf.argv()
+	.env()
+	.file({ file: __dirname + '/config.json' });
+
+nconf.defaults({
+    'remote_host': '',
+    'remote_port': 6379,
+    'local_host': '127.0.0.1',
+    'local_port': 6379
+});
+
+var remote_host = nconf.get('remote_host'),
+	remote_port = nconf.get('remote_port'),
+	local_host = nconf.get('local_host'),
+	local_port = nconf.get('local_port');
+
+console.log(remote_host);
+process.exit(0);
+
+var remote_client = redis.createClient(remote_port, remote_host),
+	local_client = redis.createClient(local_port, local_host);
+
+remote_client.send_command("psubscribe", ['*'], function(data){
+	remote_client.on('message', function(channel, message){
+        local_client.publish(channel, message);
+    });
+});
